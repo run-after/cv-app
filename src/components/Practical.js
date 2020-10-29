79e3214fe5b094b4557900d2a00d224e453322f5
run-after <run.after@protonmail.com>
2020-10-29 14:36:51 -0500

    Basic layout of Practical

--- src/components/Practical.js
+++ src/components/Practical.js
@@ -9,8 +9,32 @@ class Practical extends React.Component {
 
   render() {
     return (
-      <div className="practical">
-        <p>practical</p>
+      <div className='practical'>
+        <h3 className='title'>Experience</h3>
+        <div className="job">
+          <h4 className='job-title'>Job 1</h4>
+          <h6 className='location'>Location 1</h6>
+          <p className='dates'>1/19 - Current</p>
+          <p className='summary'>Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
+        </div>
+        <div className="job">
+          <h4 className='job-title'>Job 2</h4>
+          <h6 className='location'>Location 2</h6>
+          <p className='dates'>1/18 - 1/19</p>
+          <p className='summary'>Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
+        </div>
+        <div className="job">
+          <h4 className='job-title'>Job 3</h4>
+          <h6 className='location'>Location 3</h6>
+          <p className='dates'>1/17 - 1/18</p>
+          <p className='summary'>Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
+        </div>
+        <div className="job">
+          <h4 className='job-title'>Job 4</h4>
+          <h6 className='location'>Location 4</h6>
+          <p className='dates'>1/16 - 1/17</p>
+          <p className='summary'>Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
+        </div>
       </div>
     )
   }

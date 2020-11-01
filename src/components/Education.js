6daa3a27c61222931fbf4c298e5b4e14e28388a3
run-after <run.after@protonmail.com>
2020-11-01 12:58:54 -0600

    Basic layout of Education component

--- src/components/Education.js
+++ src/components/Education.js
@@ -9,8 +9,20 @@ class Education extends React.Component {
 
   render() {
     return (
-      <div class="education">
-        <p>education</p>
+      <div className='education'>
+        <h3 className='title'>Education</h3>
+        <div className="school">
+          <h4 className='school-name'>School 1</h4>
+          <h6 className='location'>Location 1</h6>
+          <p className='dates'>1/19 - Current</p>
+          <p className='summary'>Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
+        </div>
+        <div className="school">
+          <h4 className='school-name'>School 2</h4>
+          <h6 className='location'>Location 2</h6>
+          <p className='dates'>1/18 - 1/19</p>
+          <p className='summary'>Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
+        </div>
       </div>
     )
   }
